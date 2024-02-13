cd3f6e59dceaba8f05afe0d9334e544e4a589e31
Madi <madi_gaziz@mail.ru>
2024-02-13 12:14:54 +0600

    Practice solving logical problems

---
+++ task_6.py
@@ -0,0 +1,11 @@
+jake = int(input("Enter grade for Jake:\n"))
+
+john = int(input("Enter grade for John:\n"))
+
+jack = int(input("Enter grade for Jack:\n"))
+
+jane = int(input("Enter grade for Jane:\n"))
+
+average = (jake + john + jack + jane) / 4
+
+print(f"Mean for grades: {round(average)}")
